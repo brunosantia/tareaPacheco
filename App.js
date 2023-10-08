5681971bb4c7f8d9f15fd03eae72f03c39631712
brunosantia <95445270+brunosantia@users.noreply.github.com>
2023-10-07 19:04:38 -0600

    pacheco

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import login from "./login"
+import registro from "./registro"
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="login" options={{headerShown: false}} component={login} />
+        <Stack.Screen name="registro" options={{headerShown: false}} component={registro} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- login.js
+++ login.js
@@ -1,99 +1,101 @@
-import {styleSheet, SafeAreaView, view, image, text,TextInput,TouchableOpacity, Button } from "react-native" ;
+import {styleSheet, SafeAreaView, view, image, text,TextInput,TouchableOpacity} from "react-native" ;
 
 export default function Login(){
     return(
-        <SafeAreaView styl={styleSheet.container}>
-            <View styles={styles.firstSection}>
-                <image 
-                source={{
-                    url: "assets/FontFaceSetLoadEvent.jpg",
-                }}
-                style={{width: "100%",height: "120%", resizeMode: "cover"}}
-                />
-                <view 
-                style={{
-                    position:"absolute",
-                }}
-                >
-                <text style={style.title}>Revelo</text>
-            </view>
-
-            </View>
-            <View styles={styles.secondSection}>
-                <view style={styles.spacing}>
-                <text styles={styles.label}>email address</text>
-                <TextInput
-                style={style.TextInput}
-                underlinecolorAndroid="pink "
-                placeholder="ejemplo@ejemplo.com"
-                keyyboardType="email-address"
-                returnKeyType="next"
-                />
+        <SafeAreaView style={styleSheet.container}>
+            <view styles={styles.firstSection}>
                 </view>
-            </View>
-            <View styles={styles.secondSection}>
-                <text styles={styles.label}>password</text>
-                <TextInput
-                style={style.TextInput}
-                underlinecolorAndroid="pink"
-                placeholder="*********"
-                secureTextEntry={true}
+	
+                <view styles={styles.secondSection}>
+		<view>
+	 	 <text style={styles.label}> Email address</text>
+		 <textInput
+			style={styles.textInput}
+			underlinecolorAndroid="pink"
+			placeholder="ejemplo@ejemplo.com"
+			keyboardType= "email-address"
+			returnKeytype="next"
+		/>
+		</view>
+		<view style={styles.spacing}>
+		 <text style={styles.label}> Password</text>
+		 <textInput
+			style={styles.textInput}
+			underlinecolorAndroid="pink"
+			placeholder="***************"
+			secureTextEnttry={true}
+		/>
+		</view>
+		<view style={styles.spacing}>
+		<text style={styles.forgotPassword}>Forgot password</text>
+		</view>
+		<view style={styles.spacing}>
+		<TouchableOpacity style={styles.button}>
+		  <text style={styles.buttonText}>Login</text> 
+		</TouchableOpacity>
+		</view>
+		</view>
 
-                />
-                <text style={style.forgotPassword}>forgot password</text>
-                <button
-                title="Login"
-                styles={{
-                    backgroundcolor: "pink",
-                    padding: 8,
-                    borderRadius: 8,
-                    marginTop: 5,
-                }}
-                />
-
-            </View>
-        </SafeAreaView>
-    );
+	</SafeAreaView>
+);
 }
+
+
 export const styles = styleSheet.create({
-    container: {
-        flex: 1,
-        backgroundcolor: "#FFF",
-        alignItems: "center",
-        justifycontent: "center",
-
-    },
-    firstSetion: {
-        flex: 1,
-        backgroundcolor: "green",
-        alignItems: "center",
-        justifycontent: "center",
-        width:"100%",
-    },
-    secondSection: {
-        flex: 2,
-        backgroundcolor: "purple",
-        alignItems: "center",
-        justifycontent: "center",
-        width: "100%",
-        bordertopRightRadius: 25,
-        bordertopLeftRadius: 25,
-        padding: 20,
-    },
-    title:{
-        Fontsize: 48,
-        fntweight: "bold",
-        color: "white",
-    },
-    label:{
-        color: "#ccc",
-    },
-    TextInput: {
-        color: "black",
-
-    },
-    forgotPassword: {
-        color: "black",
-        textAling: "right",
-    }
+   contianer:{
+   flex: 1,
+   backgroundcolor: "#FFF",
+   alignItems: "center",
+   justifycontent: "center",
+},
+firstSection: {
+   flex: 1,
+   alignItems: "center",
+   justfycontent: "center",
+   backgroundcolor: "green",
+   width: "100%",
+},
+secondSection: {
+   flex: 2,
+   backgroundcolor: "#FFF",
+   width: "100%",
+   borderTopRightRadius: 25,
+   borderTopLeftRadius: 25,
+   padding: 20,
+   justifycontent: "space-between",
+},
+title:{
+   fontSize: "48",
+   color: "white",
+},
+label:{
+   color: "#CCC",
+},
+textInput:{
+   color: "#000",
+   padding: "8",
+},
+forgotPassword:{
+  color: "#000",
+textAlign: "right",
+},
+button: {
+  backgroundcolor: "pink",
+  padding: 15,
+  borderRadius: 30,
+  alignItems: "center",
+},
+buttonText:{
+  color: "white",
+  fontWeight: "bold",
+},
+spacing:{
+  marginTop: "10",
+},
+row:{
+  flexDirection: "row",
+  justifycontent: "center",
+}
 })
+
+
